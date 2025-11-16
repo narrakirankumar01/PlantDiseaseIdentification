cbee59db95caa26713b865d7d75d1275223a20ed
narrakirankumar01 <narrakirankumar01@gmail.com>
2025-11-16 22:53:15 +0530

    Designed Home Screen with option to upload the image of the plant leaves

---
+++ app/src/main/java/plantdiseaseidentifier/kirankumar/teesproject/HomeActivity.kt
@@ -0,0 +1,13 @@
+package plantdiseaseidentifier.kirankumar.teesproject
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
